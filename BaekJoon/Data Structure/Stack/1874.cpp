f266d5cded121b9794128bb195aec2a75021f1f5
JungTaeKwon <liku99@naver.com>
2022-07-19 19:18:27 +0900

    1874.cpp source file up to date

--- BaekJoon/Data Structure/Stack/1874.cpp
+++ BaekJoon/Data Structure/Stack/1874.cpp
@@ -1,39 +1,89 @@
+/*
+문제
+스택 (stack)은 기본적인 자료구조 중 하나로, 컴퓨터 프로그램을 작성할 때 자주 이용되는 개념이다. 스택은 자료를 넣는 (push) 입구와 자료를 뽑는 (pop) 입구가 같아 제일 나중에 들어간 자료가 제일 먼저 나오는 (LIFO, Last in First out) 특성을 가지고 있다.
+
+1부터 n까지의 수를 스택에 넣었다가 뽑아 늘어놓음으로써, 하나의 수열을 만들 수 있다. 이때, 스택에 push하는 순서는 반드시 오름차순을 지키도록 한다고 하자. 임의의 수열이 주어졌을 때 스택을 이용해 그 수열을 만들 수 있는지 없는지, 있다면 어떤 순서로 push와 pop 연산을 수행해야 하는지를 알아낼 수 있다. 이를 계산하는 프로그램을 작성하라.
+
+입력
+첫 줄에 n (1 ≤ n ≤ 100,000)이 주어진다. 둘째 줄부터 n개의 줄에는 수열을 이루는 1이상 n이하의 정수가 하나씩 순서대로 주어진다. 물론 같은 정수가 두 번 나오는 일은 없다.
+
+출력
+입력된 수열을 만들기 위해 필요한 연산을 한 줄에 한 개씩 출력한다. push연산은 +로, pop 연산은 -로 표현하도록 한다. 불가능한 경우 NO를 출력한다.
+
+예제 입력 1 
+8
+4
+3
+6
+8
+7
+5
+2
+1
+예제 출력 1 
++
++
++
++
+-
+-
++
++
+-
++
++
+-
+-
+-
+-
+-
+예제 입력 2 
+5
+1
+2
+5
+3
+4
+예제 출력 2 
+NO
+*/
 #include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
 	stack<int> stk;
-	int n;
-	string s;
-	scanf("%d",&n);
-	int* checkArr = new int[n];
-	for(int i = 0 ; i < n ; i++)
-	{
-		checkArr[i] = i+1;
-	}
-	int cur = 1;
+	int n,now=1;
+	cin >> n;
+	string ans = "";
 	for(int i = 0 ; i < n ; i++)
 	{
 		int ipt;
-		scanf("%d",&ipt);
-		while(cur < ipt+1)
+		cin >> ipt;
+		while(now <= ipt)
 		{
-			stk.push(cur++);
-			if(checkArr[stk.top()-1])
-				s += "+";
+			stk.push(now);
+			ans += "+";
+			now++;
 		}
-		checkArr[stk.top()-1] = 0;
-		stk.pop();
-		s += "-";
-		cur--;
-	}
-	for(int i = 0 ; i < n ; i++)
-	{
-		if(!checkArr[i])
+		if(stk.top() == ipt)
 		{
-			printf("NO\n");
+			stk.pop();
+			ans += "-";
+		}
+		else
+		{
+			cout << "NO";
 			return 0;
 		}
 	}
-	for(auto a : s) printf("%c\n",a);
-}
+	for(int i = 0 ; i < ans.size() ; i++)
+	{
+		cout << ans[i] << "\n";
+	}
+} 
+/*
+스택으로 입력받는 수열을 생성할 수 있는가에 대한 문제..
+문제 이해가 안 됐지만 스택 그림을 그려가며 해보니 문제 이해가 되었다
+핵심은 push와 pop을 할 때 이미 pop된 수들의 중복처리가 문제였지만 now변수를 활용하여 해결 
+*/
