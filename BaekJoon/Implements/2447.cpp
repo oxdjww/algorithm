ee4fb4acc426d5af4fd8cceb33577fb50a74cdd8
JungTaeKwon <liku99@naver.com>
2022-07-30 19:06:07 +0900

    BOJ 2447 upload

---
+++ BaekJoon/Implements/2447.cpp
@@ -0,0 +1,28 @@
+/*
+BOJ 2447 º°Âï±â - 10
+https://www.acmicpc.net/problem/2447
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int star(int i, int j, int n);
+
+int main( ){
+	int n;
+	scanf("%d",&n);
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++)
+			star(i, j, n);
+		printf("\n");
+	}
+}
+
+int star(int i, int j, int n){
+	if((i / n) % 3 == 1 && (j / n) % 3 == 1) 
+		cout << " ";
+	else if(n / 3 == 0)
+		cout << "*";
+	else
+		star(i, j, n / 3);	//Àç±ÍÈ£Ãâ
+	return 0;
+}
